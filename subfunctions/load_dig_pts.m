663a97a182f22474bd5c8618afe8299e29a80761
Luca Pollonini <lpollonini@uh.edu>
2018-10-09 15:41:52 -0500

    Optimized search for fiducials, sources and detectors in digitized file. Coding suggestions by Ted Huppert (thanks!)

--- subfunctions/load_dig_pts.m
+++ subfunctions/load_dig_pts.m
@@ -5,36 +5,20 @@ function [ handles ] = load_dig_pts( handles, dig_pts_path )
 dig_pts=importdata(dig_pts_path);        
 
 % Fiducials nz, ar, al to be used for transformation into predefined atlas
-for i=1:size(dig_pts.data,1)
-    match1=strfind(dig_pts.textdata(i),'nz:');
-    match2=strfind(dig_pts.textdata(i),'ar:');
-    match3=strfind(dig_pts.textdata(i),'al:');
-    fid_pts(1:3,:)=dig_pts.data(1:3,:);
-end
-% Reads all sources
-match=strfind(dig_pts.textdata,['s1:']);
-idx=find(~cellfun(@isempty,match));
-s_pts=[];
-for i=1:size(dig_pts.data,1)-idx+1
-    match=cell2mat(strfind(dig_pts.textdata(idx+i-1),['s' num2str(i) ':']));
-    if ~isempty(match)
-        s_pts=[s_pts; dig_pts.data(idx+i-1,:)];
-    end
-end
-%Reads all detectors
-match=strfind(dig_pts.textdata,['d1:']);
-idx=find(~cellfun(@isempty,match));
-d_pts=[];
-for i=1:size(dig_pts.data,1)-idx+1
-    match=cell2mat(strfind(dig_pts.textdata(idx+i-1),['d' num2str(i) ':']));
-    if ~isempty(match)
-        d_pts=[d_pts; dig_pts.data(idx+i-1,:)];
-    end
-end
+match1=find(ismember(dig_pts.textdata,'nz:'));
+match2=find(ismember(dig_pts.textdata,'ar:'));
+match3=find(ismember(dig_pts.textdata,'al:'));
+fid_pts(1:3,:)=dig_pts.data([match1 match2 match3],:);
+
+% Reads all sources and detectors
+match_s1 = find(ismember(dig_pts.textdata,'s1:'));
+match_d1 = find(ismember(dig_pts.textdata,'d1:'));
+s_pts = dig_pts.data(match_s1:match_d1-1,:);
+d_pts = dig_pts.data(match_d1:end,:);
 
 % Finds rigid transformation from Patriot space to atlas space
 [R,t] = rigid_transform_3D(fid_pts, handles.atlas_fid_pts([1 3 2],:)); % Note: fid_pts are the digitized fiducials
-%T1=[R t; 0 0 0 1];
+
 % Applies transformation to bring all points (fds, sources, detectors) into atlas space, and save them in GUI handler to pass it everywhere needed 
 handles.fid_pts = transform_points(fid_pts,R,t);
 handles.src_pts = transform_points(s_pts,R,t);
