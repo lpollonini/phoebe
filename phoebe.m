1dd714a2bc242729dadc7001e802e7dc11c5e0e8
Luca Pollonini <lpollonini@uh.edu>
2021-11-05 13:57:24 -0500

    Created function fill_lsl_buffer

--- phoebe.m
+++ phoebe.m
@@ -96,7 +96,6 @@ else
     set(handles.uibuttongroup_qc,'SelectedObject',handles.radiobutton_qchannel);
 end
 
-
 % Import digitized layout and transform into atlas space
 if ~strcmp(settings.dig_pts_path,'')
     handles = load_dig_pts(handles,settings.dig_pts_path);
@@ -304,36 +303,7 @@ end
 % If START/STOP button is START mode, run this loop indefinitely until the button is toggled
 while ishandle(hObject) && get(hObject,'Value')
     
-    % Read a chunk of data and fill the buffer (all devices)
-    while nnz(lsl_buffer(:,1)) < size(lsl_buffer,1) % if the FIFO buffer is not totally full (first few secs), wait until it fills
-        while 1
-            [chunk,~] = inlet.pull_chunk(); % Pull a chunk of fresh samples
-            if ~isempty(chunk)
-                break
-            end
-        end
-        chunk = chunk';
-        if size(chunk,1) < size(lsl_buffer,1)    % If the data chunk is smaller than the buffer
-            lsl_buffer(1:size(lsl_buffer,1)-size(chunk,1),:) = lsl_buffer(size(chunk,1)+1:end,:); % Shift up the buffer to make room 
-            lsl_buffer(size(lsl_buffer,1)-size(chunk,1)+1:end,:) = chunk; % Put chunk in buffer
-        else
-            lsl_buffer(:,:) = chunk(size(chunk,1)-size(lsl_buffer,1)+1:end,:); % Put chunk in buffer
-        end% Put chunk in buffer
-    end
-    % We are repeating the same code above to make room for new samples when the buffer is already filled from previous pulls 
-    while 1
-        [chunk,~] = inlet.pull_chunk(); % Pull a chunk of fresh samples
-        if ~isempty(chunk)
-            break
-        end
-    end
-    chunk = chunk';
-    if size(chunk,1) < size(lsl_buffer,1)    % If the data chunk is smaller than the buffer
-        lsl_buffer(1:size(lsl_buffer,1)-size(chunk,1),:) = lsl_buffer(size(chunk,1)+1:end,:); % Shift up the buffer to make room 
-        lsl_buffer(size(lsl_buffer,1)-size(chunk,1)+1:end,:) = chunk; % Put chunk in buffer
-    else
-        lsl_buffer(:,:) = chunk(size(chunk,1)-size(lsl_buffer,1)+1:end,:); % Put chunk in buffer
-    end
+    lsl_buffer = fill_lsl_buffer(lsl_buffer);
     
     % Parse the data depending on the device-specific arrangement(metadata)
     switch(get(handles.popupmenu_device,'value'))
---
+++ subfunctions/fill_lsl_buffer.m
@@ -0,0 +1,36 @@
+function [lsl_buffer] = fill_lsl_buffer(lsl_buffer)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+% Read a chunk of data and fill the buffer (all devices)
+while nnz(lsl_buffer(:,1)) < size(lsl_buffer,1) % if the FIFO buffer is not totally full (first few secs), wait until it fills
+    while 1
+        [chunk,~] = inlet.pull_chunk(); % Pull a chunk of fresh samples
+        if ~isempty(chunk)
+            break
+        end
+    end
+    chunk = chunk';
+    if size(chunk,1) < size(lsl_buffer,1)    % If the data chunk is smaller than the buffer
+        lsl_buffer(1:size(lsl_buffer,1)-size(chunk,1),:) = lsl_buffer(size(chunk,1)+1:end,:); % Shift up the buffer to make room 
+        lsl_buffer(size(lsl_buffer,1)-size(chunk,1)+1:end,:) = chunk; % Put chunk in buffer
+    else
+        lsl_buffer(:,:) = chunk(size(chunk,1)-size(lsl_buffer,1)+1:end,:); % Put chunk in buffer
+    end% Put chunk in buffer
+end
+% We are repeating the same code above to make room for new samples when the buffer is already filled from previous pulls 
+while 1
+    [chunk,~] = inlet.pull_chunk(); % Pull a chunk of fresh samples
+    if ~isempty(chunk)
+        break
+    end
+end
+chunk = chunk';
+if size(chunk,1) < size(lsl_buffer,1)    % If the data chunk is smaller than the buffer
+    lsl_buffer(1:size(lsl_buffer,1)-size(chunk,1),:) = lsl_buffer(size(chunk,1)+1:end,:); % Shift up the buffer to make room 
+    lsl_buffer(size(lsl_buffer,1)-size(chunk,1)+1:end,:) = chunk; % Put chunk in buffer
+else
+    lsl_buffer(:,:) = chunk(size(chunk,1)-size(lsl_buffer,1)+1:end,:); % Put chunk in buffer
+end
+
+end
+
