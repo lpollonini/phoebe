27dbdc747f867f51d4d9efa0eca655740e1cb251
Luca Pollonini <lpollonini@uh.edu>
2021-11-18 12:05:30 -0600

    Displays brain also on right brain

--- subfunctions/plot_atlas_empty.m
+++ subfunctions/plot_atlas_empty.m
@@ -14,6 +14,7 @@ rotate3d on
 % If double view, do the same on the right head
 if get(handles.uipanel_head,'SelectedObject')==handles.radiobutton_doubleview 
     plot_mesh(handles.axes_right,handles.faces(:,2:4), handles.vertices(:,2:4),handles.opacity);
+    plot_mesh(handles.axes_left,handles.faces_brain(:,2:4), handles.vertices_brain(:,2:4),1);
     hold(handles.axes_right,'on')
     scatter3(handles.axes_right,handles.atlas_fid_pts(:,1), handles.atlas_fid_pts(:,2), handles.atlas_fid_pts(:,3), 20,'y','fill'); % Plot atlas fiducials        axis([-130 130 -230 130 -100 150]) % FIX autoset larger view
     set(handles.axes_right,'XLim',[-90-80*handles.zoom_index 90+80*handles.zoom_index])
