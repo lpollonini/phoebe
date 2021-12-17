564ba269047e448aa5b1897ace908dcb5e90c9b8
Luca Pollonini <lpollonini@uh.edu>
2021-12-17 11:01:38 -0600

    Prevents crash during signal saturation

--- subfunctions/quality_metrics.m
+++ subfunctions/quality_metrics.m
@@ -4,7 +4,12 @@ function [sci,psp,fpsp] = quality_metrics(handles,signal1,signal2)
 
 fcut_max = str2double(get(handles.edit_highcutoff,'string'));
 similarity = xcorr(signal1,signal2,'unbiased');  %cross-correlate the two wavelength signals - both should have cardiac pulsations
-similarity = length(signal1)*similarity./sqrt(sum(abs(signal1).^2)*sum(abs(signal2).^2));  % this makes the SCI=1 at lag zero when x1=x2 AND makes the power estimate independent of signal length, amplitude and Fs
+if any(abs(similarity)>eps)
+    % this makes the SCI=1 at lag zero when x1=x2 AND makes the power estimate independent of signal length, amplitude and Fs
+    similarity = length(signal1)*similarity./sqrt(sum(abs(signal1).^2)*sum(abs(signal2).^2));  % this makes the SCI=1 at lag zero when x1=x2 AND makes the power estimate independent of signal length, amplitude and Fselse
+else
+    warning('Saturated signals');
+end
 [pxx,f] = periodogram(similarity,hamming(length(similarity)),length(similarity),handles.fs,'power');
 [pwrest,idx] = max(pxx(f<fcut_max));
 sci = similarity(length(signal1));
